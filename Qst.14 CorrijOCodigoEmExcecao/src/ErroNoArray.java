3dca3f3288aca31f450432df62d8cc7d679abecf
RikelmeJ <128755900+RikelmeJ@users.noreply.github.com>
2023-04-30 16:47:35 -0300

    Qst.14 Corrigir o codigo com excecao.java

---
+++ Qst.14 CorrijOCodigoEmExcecao/src/ErroNoArray.java
@@ -0,0 +1,13 @@
+
+public class ErroNoArray { public static void main(String[] args) {
+    int[] vetor = new int[] { 2, 4, 6, 8, 10, 12, 14 };
+
+    for (int i = 0; i < vetor.length; i++)
+
+        System.out.println(vetor[i]);
+}
+
+}
+
+//Adicionei o mais um elemento que é o 14
+//Alterei a repetição por um vetor com a propriedade length
